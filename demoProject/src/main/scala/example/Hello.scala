da1f66627fd9a611a81273ad079fe10625d1df6c
Chris André <chris@chris-andre.fr>
2020-09-17 21:49:22 -0400

    Handling potential errors

--- demoProject/src/main/scala/example/Hello.scala
+++ demoProject/src/main/scala/example/Hello.scala
@@ -5,11 +5,22 @@ import zio._
 import zio.console._
 
 object Hello extends App {
-  def run(args: List[String]) = printAnswerToLife *> ZIO.succeed(
-    ExitCode(0)
-  )
+  def run(args: List[String]) =
+    askForAValueAndMultiplyBy2.fold(
+      _ => ExitCode(1),
+      _ => ExitCode(0)
+    )
 
   val answerToLife = IO.succeed(21).map(_ * 2)
 
-  val printAnswerToLife = answerToLife.flatMap(answer => putStrLn(s"The answer to Life, the Universe and Everything is $answer"))
+  val printAnswerToLife = answerToLife.flatMap(answer =>
+    putStrLn(s"The answer to Life, the Universe and Everything is $answer")
+  )
+
+  val askForAValueAndMultiplyBy2: ZIO[Console, Throwable, Unit] = for {
+    _ <- putStrLn("Hello! What value do you want to multiply by 2?")
+    numberStr <- getStrLn
+    number <- ZIO(numberStr.toInt * 2)
+    _ <- putStrLn(s"'$numberStr' multiplied by 2 is '$number'")
+  } yield ()
 }
