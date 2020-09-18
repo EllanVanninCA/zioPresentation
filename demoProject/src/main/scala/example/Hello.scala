409dd2c6b9ca348e2a40598574020e02fcdcb7f7
Chris André <chris@chris-andre.fr>
2020-09-17 21:33:42 -0400

    Example of displaying a value to the screen

--- demoProject/src/main/scala/example/Hello.scala
+++ demoProject/src/main/scala/example/Hello.scala
@@ -2,10 +2,14 @@ package example
 
 import zio.App
 import zio._
+import zio.console._
 
 object Hello extends App {
-  def run(args: List[String]) =
-    ZIO.succeed(
-      ExitCode(0)
-    )
+  def run(args: List[String]) = printAnswerToLife *> ZIO.succeed(
+    ExitCode(0)
+  )
+
+  val answerToLife = IO.succeed(21).map(_ * 2)
+
+  val printAnswerToLife = answerToLife.flatMap(answer => putStrLn(s"The answer to Life, the Universe and Everything is $answer"))
 }
